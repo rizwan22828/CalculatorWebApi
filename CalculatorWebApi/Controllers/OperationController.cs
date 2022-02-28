565ac9f054873b9b3d014147c25fb4fb705b2881
rizwan.zaman <rizwan.zaman@ISBLT-9259>
2022-02-28 16:50:39 +0500

    Add project files.

---
+++ CalculatorWebApi/Controllers/OperationController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CalculateAddition(string numbers)
+        {
+            return Ok();
+        }
+
+        //[HttpGet]
+        //public IActionResult CalculateAddition(string numbers)
+        //{
+        //    return Ok();
+        //}
+        //[HttpGet]
+        //public IActionResult CalculateAddition(string numbers)
+        //{
+        //    return Ok();
+        //}
+        //[HttpGet]
+        //public IActionResult CalculateAddition(string numbers)
+        //{
+        //    return Ok();
+        //}
+        //[HttpGet]
+        //public IActionResult CalculateAddition(string numbers)
+        //{
+        //    return Ok();
+        //}
+
+    }
+}
